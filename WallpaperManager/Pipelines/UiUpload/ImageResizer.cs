da3e76c9ffb530da96bd77ee306faa8af0e1cb11
sobek1985 <sobek1985@gmail.com>
2015-10-27 13:34:51 +0000

    Init Image Resize pipeline work

---
+++ WallpaperManager/Pipelines/UiUpload/ImageResizer.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Drawing;
+
+namespace MikeRobbins.WallpaperManager.Pipelines.UiUpload
+{
+    public class ImageResizer
+    {
+        public Image ResizeImage(Image image, int maxWidth, int maxHeight)
+        {
+            double ratioX = (double)maxWidth / image.Width;
+            double ratioY = (double)maxHeight / image.Height;
+            double ratio = Math.Min(ratioX, ratioY);
+
+            var targetWidth = GetTargetSize(image.Width, ratio);
+            var targetHeight = GetTargetSize(image.Height, ratio);
+
+            var resizedImage = new Bitmap(targetWidth, targetHeight);
+
+            using (Graphics graphics = Graphics.FromImage(resizedImage))
+            {
+                graphics.DrawImage(image, (int) 0, (int) 0, (int) targetWidth, (int) targetHeight);
+            }
+
+            return resizedImage;
+        }
+
+        private int GetTargetSize(int size, double ratio)
+        {
+            return (int)(size * ratio);
+        }
+    }
+}
---
+++ WallpaperManager/Pipelines/UiUpload/ResizeWallpaper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Web;
+using Sitecore.Data.Items;
+using Sitecore.Pipelines.Upload;
+
+namespace MikeRobbins.WallpaperManager.Pipelines.UiUpload
+{
+    public class ResizeWallpaper : UploadProcessor
+    {
+        //TODO: DI this
+        private readonly ImageResizer _imageResizer = new ImageResizer();
+
+        public void Process(UploadArgs args)
+        {
+            Sitecore.Diagnostics.Log.Info("Process Resize Wallpapers:", this);
+
+            foreach (MediaItem uploadedItem in args.UploadedItems)
+            {
+                //TODO: Check this comes from wallpaper manager
+                Image image = Image.FromStream(uploadedItem.GetMediaStream());
+
+
+                //TODO: Get Sizes from setting file
+                Image resizedImage = _imageResizer.ResizeImage(image, 1000, 500);
+
+                //TODO: Update File on uploadedItem;
+            }
+        }
+    }
+}
